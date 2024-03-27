e15ea46638aa08021b0329bd9dbaaf250e5df592
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 02:32:03 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,5 +1,5 @@
 import './css/style.css'
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
     const [newThing, setNewThing] = useState('')
