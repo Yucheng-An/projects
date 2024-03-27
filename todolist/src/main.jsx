6515b8858b1fa97344bac4295cb879c0bbec255b
Yucheng-An <yuc.an2019@gmail.com>
2024-03-26 17:50:15 -0700

    Auto-commit JSX on save for Project

--- todolist/src/main.jsx
+++ todolist/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
-import myTitle from "./components/myTitle.jsx";
+import MyTitle from "./components/myTitle.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <myTitle/>
+      <MyTitle/>
     <App />
   </React.StrictMode>
 )
