170cb82bae2668a04847e9c2c4776221797f2e4c
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 03:00:50 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
     const [newThing, setNewThing] = useState('A new thing...')
     const thing = []
     const handleAddChange = (event) =>{
-        console.log(event.target.value)
         setNewThing(event.target.value)
     }
     const addThing = (event) =>{
