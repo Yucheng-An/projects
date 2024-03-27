128eae8d510dd7fe70d4c8ccd87ec18510b95a2a
Yucheng-An <yuc.an2019@gmail.com>
2024-03-26 17:38:34 -0700

    Auto-commit JSX on save for Project

--- todolist/src/components/Title.jsx
+++ todolist/src/components/Title.jsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 const MyComponent = () => (
     <div>
         <Helmet>
-            <title>To_do_</title>
+            <title>To Do List</title>
         </Helmet>
     </div>
 );
