984386cbe8a2d2eb9e0577f95cda17e8c9efc961
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 03:11:01 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
             </form>
             <ul>
                 {things.map(thing=>
-                    <Thing key={thing.id} content={thing}/>
+                    <Thing key={thing.id} conten={thing}/>
                 )}
             </ul>
         </div>
