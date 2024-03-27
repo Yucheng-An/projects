453e5d76f7c636d4cb493611ec382c4906d3644b
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 02:53:52 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     const addThing = (event) =>{
         event.preventDefault()
         console.log('button clicked', event.target)
+        
     }
 
     const CreateNew = () =>{
