4c11074cabea307bc0ebf1845648945b7d5ac233
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 02:27:41 -0700

    Auto-commit JSX on save for Project

--- todolist/src/main.jsx
+++ todolist/src/main.jsx
@@ -4,6 +4,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import MyTitle from "./components/myTitle.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-      <App />
+ReactDOM.createRoot(document.getElementById('root')).render(<App />
 )
