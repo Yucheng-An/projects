f9fc762d5265e6eebec3cec7e5e6801ce55e1b26
Yucheng-An <yuc.an2019@gmail.com>
2024-03-27 02:19:52 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,17 +1,16 @@
 import './css/style.css'
 
-const CreateNew = () =>{
-    return(
-        <div >
-            <p>New to do list</p>
-            <input />
-        </div>
-    )
-}
 
 function App() {
     //const [newThing, setNewThing] = useState()
-
+    const CreateNew = () =>{
+        return(
+            <div >
+                <p>New to do list</p>
+                <input />
+            </div>
+        )
+    }
   return (
       <div>
           <h1>To Do List</h1>
