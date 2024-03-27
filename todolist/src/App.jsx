0cd54341c26c74ecab535973ab0da231ddcae3c0
Yucheng-An <yuc.an2019@gmail.com>
2024-03-26 18:04:58 -0700

    Auto-commit JSX on save for Project

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         return(
             <div >
                 <p>New to do list</p>
-                <p>
+                <p> Create new 
                     <input></input>
                 </p>
             </div>
